b91ec5a18e9ab2054fb6bd36c83a82a804c07cb3
Alfonso Guerra <huperniketes@gmail.com>
2024-06-17 01:30:51 -0400

    Removed more of the T3 starter code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,30 +15,7 @@ export default async function Home() {
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <SimGame />
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
