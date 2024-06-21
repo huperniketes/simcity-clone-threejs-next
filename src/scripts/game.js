7f4fa12440f9ffedf23876105f3550971851cfe1
Alfonso Guerra <huperniketes@gmail.com>
2024-06-21 08:09:04 -0400

    Disabled code to insert sample threejs code from https://dev.to/omher/how-to-start-using-react-and-threejs-in-a-few-minutes-2h6g to debug WebGL rendering in React.

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -53,6 +53,8 @@ export class Game {
     this.city = city;
     this.#window = aWindow;
     this.#gameWindowRef = aGameWindowRef;
+
+    if(false) {
     this.renderer = new THREE.WebGLRenderer({ 
       antialias: true
     });
@@ -87,6 +89,31 @@ export class Game {
     });
 
     window.addEventListener('resize', this.onResize.bind(this), false);
+    }
+    else
+    {
+      // Code swiped from https://dev.to/omher/how-to-start-using-react-and-threejs-in-a-few-minutes-2h6g
+      var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      var scene = new THREE.Scene();
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      // document.body.appendChild( renderer.domElement );
+      // use ref as a mount point of the Three.js scene instead of the document.body
+      aGameWindowRef.current?.appendChild( renderer.domElement );
+
+      var geometry = new THREE.BoxGeometry(1, 1, 1);
+      var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      var cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      camera.position.z = 5;
+      var animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      };
+      animate();
+    }
   }
 
   /**
