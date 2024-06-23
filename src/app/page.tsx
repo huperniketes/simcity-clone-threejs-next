1064aa681cff5e150f60d16e25259849b44cc7f8
Alfonso Guerra <huperniketes@gmail.com>
2024-06-23 02:24:03 -0400

    Removed unused directives in the page file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
 import { SimGame } from "~/simgame/simgame";
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
